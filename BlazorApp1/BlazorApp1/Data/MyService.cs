66e28fd0ffe3ddd450c3d9a2cb192932d83ae29f
purnima-equitec <155628470+purnima-equitec@users.noreply.github.com>
2024-01-15 20:31:27 +0530

    init

---
+++ BlazorApp1/BlazorApp1/Data/MyService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class MyService
+    {
+        private readonly DapperDBContext dbContext;
+
+        public MyService(DapperDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            List<Employee> emp= await dbContext.Employees.FromSqlRaw("EXEC EmployeeViewAll").ToListAsync();
+            return emp ?? new List<Employee>();
+        }
+    } 
+}
