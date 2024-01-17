b5b0ce859416e59b99d48749a3d938d5fcfc0ebf
purnima-equitec <155628470+purnima-equitec@users.noreply.github.com>
2024-01-17 10:46:25 +0530

    commit

--- BlazorApp1/BlazorApp1/Data/MyService.cs
+++ BlazorApp1/BlazorApp1/Data/MyService.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorApp1.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp1.Data
@@ -14,8 +15,54 @@ namespace BlazorApp1.Data
 
         public async Task<List<Employee>> GetEmployeesAsync()
         {
-            List<Employee> emp= await dbContext.Employees.FromSqlRaw("EXEC EmployeeViewAll").ToListAsync();
+            List<Employee> emp = await dbContext.Employees.FromSqlRaw("EXEC EmployeeViewAll").ToListAsync();
             return emp ?? new List<Employee>();
         }
-    } 
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            dbContext.Procedures.EmployeeAddOrEditAsync(employee.Empid, employee.Empname,employee.EmpDesignation,employee.EmpSalary,employee.EmpGender,employee.EmpEmail,employee.EmpAge,employee.EmpSkills);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteEmployeeAsync(int empId)
+        {
+            var result = await dbContext.Procedures.EmployeeDeleteByIDAsync(empId);
+            return result;
+        }
+
+        public async Task<EmployeeViewByIDResult> DetailsEmployeeAsync(int empId)
+        {
+            var result = await dbContext.Procedures.EmployeeViewByIDAsync(empId);
+            var employee = result.FirstOrDefault(e => e.EMPID == empId);
+            return employee;
+        }
+
+        public async Task<Employee> GetEmployeeByIdAsync(int empId)
+        {
+            Employee employee=dbContext.Employees.Where(x => x.Empid== empId).FirstOrDefault();
+            if (employee == null)
+            {
+                return null;
+            }
+            return employee;
+        }
+        public async Task EditEmployeeAsync(Employee employee)
+        {
+            dbContext.Procedures.EmployeeAddOrEditAsync(employee.Empid, employee.Empname, employee.EmpDesignation, employee.EmpSalary, employee.EmpGender, employee.EmpEmail, employee.EmpAge, employee.EmpSkills);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Employee>> GetDeletedEmployeeAsync()
+        {
+            List<Employee> emp = await dbContext.Employees.FromSqlRaw("EXEC GetAllDeletedEmployees").ToListAsync();
+            return emp ?? new List<Employee>();
+        }
+
+        public async Task RestoreEmployeeAsync(Employee emp)
+        {
+            await dbContext.Procedures.RestoreEmployeeAsync(emp.Empid);
+            await dbContext.SaveChangesAsync();
+        }
+    }
 }
